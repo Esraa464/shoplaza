e0352a7ac492f425ffff757928d7bb98037b793d
Esraa Mansour <esraamansour3398@gmail.com>
2021-09-29 19:49:06 +0200

    .

---
+++ lib/views/search/cubit.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shoplaza/const/variables.dart';
+import 'package:shoplaza/views/homeView/models/banner_and_product_model.dart';
+import 'package:shoplaza/views/homeView/states/home_states.dart';
+import 'package:shoplaza/views/search/model.dart';
+import 'package:shoplaza/views/search/states.dart';
+
+class SearchController extends Cubit<SearchStates> {
+  SearchController() : super(SearchInitial());
+
+  static SearchController of(context) => BlocProvider.of(context);
+
+  SearchModel searchModel;
+
+  Future<void> search(String text) async {
+    emit(SearchLoading());
+    try {
+      final response = await Dio().post(BASE_URL + 'products/search',
+          options: dioOptions, data: {"text": text});
+      searchModel = SearchModel.fromJson(response.data);
+      // print(searchModel.data.toJson());
+      print(response.data);
+    } catch (e, s) {
+      print(e);
+      print(s);
+    }
+    emit(SearchInitial());
+  }
+}
---
+++ lib/views/search/states.dart
@@ -0,0 +1,5 @@
+abstract class SearchStates {}
+
+class SearchInitial extends SearchStates {}
+
+class SearchLoading extends SearchStates {}
